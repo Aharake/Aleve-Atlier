77796675afa0aa19df22937fab62630d5790ec78
aharake <202112512@ua.edu.lb>
2026-05-04 10:42:42 +0300

    n

--- aleve-atlier/src/components/BackgroundShaders.jsx
+++ aleve-atlier/src/components/BackgroundShaders.jsx
@@ -1,10 +1,15 @@
+import { MeshGradient } from "@paper-design/shaders-react"
 import './BackgroundShaders.css'
 
 export default function BackgroundShaders() {
   return (
     <div className="bg-shell">
-      <div className="bg-orb bg-orb-1" />
-      <div className="bg-orb bg-orb-2" />
+      <MeshGradient
+        style={{ width: '100%', height: '100%', position: 'absolute', inset: 0 }}
+        colors={["#242424", "#1f1f1f", "#2f2f2f", "#f0f0f0"]}
+        speed={0.3}
+        backgroundColor="#242424"
+      />
       <div className="bg-tagline">Engineered to Elevate.</div>
     </div>
   )
